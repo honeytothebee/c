3203cf20a2755888c1d5b9ea93309a8e77a11979
publi <publi@192.168.154.1>
2022-11-13 14:21:33 +0900

    dfdf

---
+++ unitsoft/11-13 문자열 배열 3/main.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+int main()  //문자열 정수 변환
+{
+    printf( "%d\n", atoi( "-567"));
+    printf( "%d\n", atoi( "\n-567"));
+
+    printf( "%lld\n", atol( "-567"));
+}
+*/
+
+
+
+
+
+
+/*
+int main(){           //문자열의 문자 길이 체크
+
+ char string[50] = "This is a simple string";
+ int nStrLen;
+
+ nStrLen = strlen(string);
+ printf("%d", nStrLen);
+
+ return 0;
+
+
+}
+*/
+
+
+
+
+/*
+int main()                //문자열에 특정한 문자가 가장 먼저 나타나는 곳을 찾는다!
+{
+  char *strTemp;
+  char string[] = "This is a good car!";
+
+  strTemp = strchr( string, 'a' );
+  printf( "%s \n", strTemp );
+
+  strTemp = strchr( string, 0x67 ); //0x67 : g
+  printf( "%s \n", strTemp );
+
+  strTemp = strchr( string, 'c' );
+  printf( "%s \n", strTemp );
+
+  return 0;
+
+}
+*/
