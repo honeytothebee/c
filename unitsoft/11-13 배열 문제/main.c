0f1eb8577cc9731c784e99a38bd8eb437266ffd5
publi <publi@192.168.154.1>
2022-11-13 15:25:24 +0900

    dfd

---
+++ unitsoft/11-13 배열 문제/main.c
@@ -0,0 +1,72 @@
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>  //난수
+
+int main(){
+ int a[10],i;
+ srand(time(NULL));
+ for(i=0; i<10; i++){
+    a[i]=rand()%20+1;
+    printf("%d ",a[i]);
+ }
+
+
+}
+*/
+
+
+/*
+int main(){
+ float a[5],max=0,min=100;
+ int i;
+ for(i=0; i<5; i++){
+    scanf("%f",&a[i]);
+    if(max<a[i])
+        max=a[i];
+    if(min>a[i])
+        min=a[i];
+ }
+  printf("%.1fkg",max-min);
+}
+*/
+
+/*
+int main(){
+    int a[5],i,sum=0;
+    for(i=0; i<5; i++){
+        scanf("%d",&a[i]);
+        sum+=a[i];
+    }
+    printf("평균 신장: %.1f",(float)sum/5);
+}
+*/
+
+/*
+int main(){
+
+  int a[5],max=0,min=100,i;
+  for(i=0; i<5; i++){
+        scanf("%d",&a[i]);
+        if(max<a[i])
+        max=a[i];
+        if(min>a[i])
+            min=a[i];
+
+  }
+  printf("최대값:%d 최소값:%d",max,min);
+
+}
+*/
+
+/*
+int main()
+{
+    int a[5], sum=0;
+    for(int i=0; i<5; i++){
+            scanf("%d",&a[i]);
+        sum+=a[i];
+    }
+    printf("총합:%d 평균:%d",sum,sum/5);
+}
+*/
