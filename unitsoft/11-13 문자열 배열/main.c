d1dde64e7897fc6ac3ebb065a144c9aa4a1b8fb8
publi <publi@192.168.154.1>
2022-11-13 13:43:45 +0900

    d

---
+++ unitsoft/11-13 문자열 배열/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ int main()
+ {
+     char a[3][10];
+     int i,j,cnt=0;
+     for(int i=0; i<3; i++)
+        gets( a[i] );
+     for(i=0; i<3; i++){
+        cnt=0;
+        for(j=0; a[i][j]!='\0'; j++)
+            cnt++;
+        printf("%d 줄 문자열 길이 : %d \n",i,cnt);
+     }
+ }
+ */
+/*
+int main(){
+    char a[3][10];
+    int i,j;
+    for(int i=0; i<3; i++)
+        gets( a[i] );
+    for(i=0; i<3; i++){
+        for(j=0; a[i][j]!='\0'; j++)
+            printf("%2c",a[i][j]);
+        printf("\n");
+    }
+
+}
+*/
+
+/*
+int main()
+{
+    char a[3][10]={"korea","china","America"};
+    int i;
+    for(i=0; i<3; i++)
+        printf("%s \n",a[i]);
+}
+*/
